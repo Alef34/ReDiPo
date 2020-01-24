4f5d2ae04afbfe66c386e464c157e90cfae83b2a
Julo Jenis <julojenis@hotmail.com>
2020-01-24 09:24:49 +0100

    Add project files.

---
+++ AppRedipo/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppRedipo
+{
+    public partial class Form1 : Form
+    {
+
+        DataSet DS = new DataSet();
+        SqlDataAdapter DA = new SqlDataAdapter();
+        SqlConnection conn = new SqlConnection("Data Source=USER-02-C2\\SQLEXPRESS;Integrated Security=False;Initial Catalog=Redipo; User ID=sa;Password=;Pooling=False");
+        SqlCommand cmd;
+        string retSql = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolStripButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            NaplnDb();
+        }
+
+        private void NaplnDb()
+        {
+            retSql = @"SELECT        dbo.JEDLA.IdJ, dbo.JEDLA.Jedlo, dbo.SUROVINY.IdS, dbo.SUROVINY.Surovina, dbo.RECEPTURY.D0, dbo.RECEPTURY.D1, dbo.RECEPTURY.D2, dbo.RECEPTURY.D3, dbo.RECEPTURY.D4, dbo.RECEPTURY.D5, 
+                         dbo.RECEPTURY.D6, dbo.RECEPTURY.D8, dbo.RECEPTURY.D9, dbo.RECEPTURY.D10, dbo.RECEPTURY.D11
+                    FROM            dbo.SUROVINY INNER JOIN
+                         dbo.RECEPTURY ON dbo.SUROVINY.IdS = dbo.RECEPTURY.IdSFK RIGHT OUTER JOIN
+                         dbo.JEDLA ON dbo.RECEPTURY.IdJFK = dbo.JEDLA.IdJ";
+
+            retSql = "Select * from Jedla";
+            cmd = new SqlCommand(retSql, conn);
+            cmd.CommandType = CommandType.Text;
+            DA.SelectCommand = cmd;
+            if (DS.Tables["Jedla"] != null) DS.Tables["Jedla"].Clear();
+            DA.Fill(DS, "Jedla");
+
+            dataGridView1.DataSource = DS;
+            dataGridView1.DataMember = "Jedla";
+        }
+
+        private void TxtFilterS_TextChanged(object sender, EventArgs e)
+        {
+            DataView dv = new DataView(DS.Tables["Jedla"]);
+            dv.RowFilter = "Surovina like '%" + txtFilterS.Text + "%' ";
+        }
+
+        private void ToolStripButton2_Click(object sender, EventArgs e)
+        {
+            retSql = "Insert Into Jedla (Jedlo) Values ('Nove jedlo')";
+            cmd = new SqlCommand(retSql, conn);
+            try
+            {
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+                NaplnDb();
+            }
+            catch (Exception ex )
+            {
+                if (cmd.Connection.State != ConnectionState.Closed) cmd.Connection.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ToolStripButton3_Click(object sender, EventArgs e)
+        {
+            frmSuroviny frmSur = new frmSuroviny();
+            frmSur.StartPosition = FormStartPosition.CenterScreen;
+            frmSur.ShowDialog();
+            frmSur.Dispose();
+        }
+
+        private void TxtFilterS_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AppRedipo/frmSuroviny.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppRedipo
+{
+    public partial class frmSuroviny : Form
+    {
+
+        DataSet DS = new DataSet();
+        SqlDataAdapter DA = new SqlDataAdapter();
+        SqlConnection conn = new SqlConnection("Data Source=USER-02-C2\\SQLEXPRESS;Integrated Security=False;Initial Catalog=Redipo; User ID=sa;Password=;Pooling=False");
+        SqlCommand cmd;
+        string retSql = "";
+
+
+        public frmSuroviny()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmSuroviny_Load(object sender, EventArgs e)
+        {
+            NaplnDb();
+        }
+
+
+        private void NaplnDb()
+        {
+            retSql = "Select * from Suroviny";
+            cmd = new SqlCommand(retSql, conn);
+            cmd.CommandType = CommandType.Text;
+            DA.SelectCommand = cmd;
+            if (DS.Tables["Suroviny"] != null) DS.Tables["Suroviny"].Clear();
+            DA.Fill(DS, "Suroviny");
+
+            dataGridView1.DataSource = DS;
+            dataGridView1.DataMember = "Suroviny";
+        }
+
+        private void ToolStripButton2_Click(object sender, EventArgs e)
+        {
+            retSql = "Insert Into Suroviny (Surovina) Values ('Nova surovina')";
+            cmd = new SqlCommand(retSql, conn);
+            try
+            {
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+                NaplnDb();
+            }
+            catch (Exception ex)
+            {
+                if (cmd.Connection.State != ConnectionState.Closed) cmd.Connection.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ToolStripButton3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0) return;
+            int ids = (int)dataGridView1.SelectedRows[0].Cells["IdS"].Value;
+            retSql = "Update Suroviny Set Surovina = '"+ txtSurovina.Text  +"' Where IdS="+ ids;
+            cmd = new SqlCommand(retSql, conn);
+            try
+            {
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+                NaplnDb();
+            }
+            catch (Exception ex)
+            {
+                if (cmd.Connection.State != ConnectionState.Closed) cmd.Connection.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+                txtSurovina.Text = (string)dataGridView1.SelectedRows[0].Cells["Surovina"].Value;
+        }
+    }
+}
